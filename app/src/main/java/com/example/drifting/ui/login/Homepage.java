8078a2a1744c7f0d38e997752ad3fc23edd5af79
Jiaming Yang <alex760723501@gmail.com>
2020-11-04 02:24:35 -0800

    added random bottle image generator

--- app/src/main/java/com/example/drifting/ui/login/Homepage.java
+++ app/src/main/java/com/example/drifting/ui/login/Homepage.java
@@ -3,8 +3,15 @@ package com.example.drifting.ui.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.drifting.R;
+
+import java.util.Random;
 
 public class Homepage extends AppCompatActivity {
 
@@ -12,5 +19,36 @@ public class Homepage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        final ImageView myimg = findViewById(R.id.imageView7);
+
+
+        myimg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myimg.setImageResource(getRandomBottleImg());
+                Log.v(getRandomBottleImg()+"", getRandomBottleImg()+"");
+            }
+        });
+
+    }
+
+    public int getRandomBottleImg(){
+        int bottle;
+        Random rand = new Random();
+        int[] bottleAry = {R.drawable.bottle1, R.drawable.bottle2, R.drawable.bottle3,
+                R.drawable.bottle4};
+
+        bottle = bottleAry[rand.nextInt(bottleAry.length)];
+        return bottle;
+    }
+
+    public int getRandomBottleLocation(){
+        int location;
+        Random rand = new Random();
+        int[] bottleAry = {};
+
+        location = bottleAry[rand.nextInt(bottleAry.length)];
+        return location;
     }
 }
