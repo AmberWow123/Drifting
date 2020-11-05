debf4c64fbb82fbed3b3391e6d83cfe24cbdb1d6
Jiaming Yang <alex760723501@gmail.com>
2020-11-05 02:36:45 -0800

    finished animation on bottles

--- app/src/main/java/com/example/drifting/ui/login/HomepageActivity.java
+++ app/src/main/java/com/example/drifting/ui/login/HomepageActivity.java
@@ -3,6 +3,7 @@ package com.example.drifting.ui.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -19,14 +20,18 @@ public class HomepageActivity extends AppCompatActivity {
     public static int[] bottleAry = {R.id.imageView1, R.id.imageView2, R.id.imageView3, R.id.imageView4,
             R.id.imageView5, R.id.imageView6, R.id.imageView7};
     // define bottle images
-    public static int[] imgAry = {R.drawable.bottle1, R.drawable.bottle2, R.drawable.bottle3,
-            R.drawable.bottle4};
+    public static int[] imgAry = {R.drawable.animated_bottle1, R.drawable.animated_bottle2, R.drawable.animated_bottle3,
+            R.drawable.animated_bottle4};
+
     static boolean[] availableLocation =  {false,false,false,false,false,false,false};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
 
         final Button generate_button = findViewById(R.id.generate_button);
         ImageView[] bottles = new ImageView[7];
@@ -41,29 +46,26 @@ public class HomepageActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Bottle bottle1 = new Bottle("123");
                 bottle1.setVisible();
+                //bottle1.bottleAnimation.start();
 
                 Bottle bottle2 = new Bottle("123");
                 bottle2.setVisible();
+               // bottle2.bottleAnimation.start();
 
                 Bottle bottle3 = new Bottle("123");
                 bottle3.setVisible();
+               // bottle3.bottleAnimation.start();
             }
         });
 
 
-
-
-
-
-
-
-
     }
 
-
-
-
-
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        //myanimation.start();
+    }
 
     public class Bottle{
 
@@ -71,13 +73,17 @@ public class HomepageActivity extends AppCompatActivity {
         ImageView bottleLocation;
         int imageSrc;
         int index;
+        AnimationDrawable bottleAnimation;
 
         // construct with a message
         public Bottle(String msg){
             message = msg;
             bottleLocation =  findViewById(getRandomBottleLocation());
             imageSrc = getRandomBottleImg();
-            bottleLocation.setImageResource(imageSrc);
+            bottleLocation.setBackgroundResource(imageSrc);
+
+            bottleAnimation = (AnimationDrawable) bottleLocation.getBackground();
+            bottleAnimation.start();
 
             bottleLocation.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -85,6 +91,7 @@ public class HomepageActivity extends AppCompatActivity {
 
                     startActivity(new Intent(HomepageActivity.this, ViewBottleActivity.class));
                     availableLocation[index] = false;
+                    bottleAnimation.stop();
                     bottleLocation.setVisibility(View.GONE);
                 }
             });
