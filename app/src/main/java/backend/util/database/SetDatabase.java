88a9563d29f6e71b121be7c543018725e348fb36
VictorTao1998 <jit124@ucsd.edu>
2020-11-08 03:54:04 -0800

    user_database

---
+++ app/src/main/java/backend/util/database/SetDatabase.java
@@ -0,0 +1,15 @@
+package backend.util.database;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class SetDatabase {
+    private DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+
+    public void addNewUser(UserProfile userProfile) {
+        //System.out.println(database == null);
+        DatabaseReference usersRef = database.child("user");
+
+        usersRef.child(userProfile.user_id).setValue(userProfile);
+    }
+}
