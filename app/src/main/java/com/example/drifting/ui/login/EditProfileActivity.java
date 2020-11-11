1db863760ee2bade29ffe2fc28267a29e3e684bb
Tao Jin <tajin@ucsd.edu>
2020-11-10 16:28:30 -0800

    changed edit profile page

--- app/src/main/java/com/example/drifting/ui/login/EditProfileActivity.java
+++ app/src/main/java/com/example/drifting/ui/login/EditProfileActivity.java
@@ -18,44 +18,24 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class ProfileActivity extends AppCompatActivity {
+public class EditProfileActivity extends AppCompatActivity {
 
-    Button publicpre;
     int flag;
     Button editButton;
-    Button privatepre;
-
-    public void startEditActivity() {
-        Intent intent = new Intent(this, EditProfileActivity.class);
+    public void finishEditActivity() {
+        Intent intent = new Intent(this, ProfileActivity.class);
         startActivity(intent);
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.activity_edit_profile);
 
-        publicpre = findViewById(R.id.publicpre);
-        privatepre = findViewById(R.id.privatepre);
-        flag = 0;
         editButton = findViewById(R.id.editbutton);
         editButton.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startEditActivity();
-            }
-        });
-        publicpre.setOnClickListener(new Button.OnClickListener() {
-            public void onClick (View v){
-                switch (flag) {
-                    case 0:
-                        publicpre.setActivated(false);
-                        flag = 1;
-                        break;
-                    case 1:
-                        publicpre.setActivated(true);
-                        flag = 0;
-                        break;
-                }
+                finishEditActivity();
             }
         });
     }
