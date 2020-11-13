52b9c0b43b6c908933df17545dbf7399c3411ab7
Vickie <49846243+yuc434@users.noreply.github.com>
2020-11-12 23:48:12 -0800

    fix bug

--- app/src/main/java/com/example/drifting/ui/login/RegisterActivity.java
+++ app/src/main/java/com/example/drifting/ui/login/RegisterActivity.java
@@ -38,10 +38,10 @@ public class RegisterActivity extends AppCompatActivity {
 
         //TODO: add these code after we have sign out button
        //check if the user is already logged in
-       if(fAuth.getCurrentUser() != null){
-           startActivity(new Intent(getApplicationContext(), MainActivity.class));
-           finish();
-       }
+//        if(fAuth.getCurrentUser() != null){
+//            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+//            finish();
+//        }
 
         mRegisterBtn.setOnClickListener((new View.OnClickListener() {
             @Override
