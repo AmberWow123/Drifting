3f2fcf427dcbf837caa1edcbb7c1a4fb076cdc82
AmberWow123 <eatingwow@gmail.com>
2020-11-13 04:52:13 -0800

    added "adding image" feature
    rough feature, still need to modified

--- app/src/main/java/com/example/drifting/ui/login/WriteMessageActivity.java
+++ app/src/main/java/com/example/drifting/ui/login/WriteMessageActivity.java
@@ -1,18 +1,27 @@
 package com.example.drifting.ui.login;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,12 +35,96 @@ public class WriteMessageActivity extends AppCompatActivity {
     private static final int MY_PERMISSION_REQUEST_LOCATION = 1;
     TextView locationText;
 
+    Switch switch_anon;
+    TextView text_view_anon;
+
+    private static String MY_PREFS = "switch_prefs";
+    private static String ANON_STATUS = "anon_on";
+    private static String SWITCH_STATUS = "switch_status";
+
+    boolean switch_status;
+    boolean anon_status;
+
+    SharedPreferences myPreferences;
+    SharedPreferences.Editor myEditor;
+
+    // for adding image
+    ImageView added_image_view;
+    Button added_image_button;
+
+    private static final int IMAGE_PICK_CODE = 1000;
+    private static final int PERMISSION_CODE = 1001;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_message);
         locationText = findViewById(R.id.get_location_text);
 
+        // adding image
+        added_image_view = findViewById(R.id.image_view_added);
+        added_image_button = findViewById(R.id.button_Add_Image);
+
+        added_image_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // check runtime permission
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                        // permission not granted, request it.
+                        String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
+                        // show popup for runtime permission
+                        requestPermissions(permissions, PERMISSION_CODE);
+                    } else {
+                        // permission already granted
+                        pickImageFromGallery();
+                    }
+                }
+                else {
+                    // system os is less than marshmallow
+                    pickImageFromGallery();
+                }
+            }
+        });
+
+        // switch button
+        switch_anon = findViewById(R.id.switch_button);
+        text_view_anon = findViewById(R.id.text_is_anon);
+
+        myPreferences = getSharedPreferences(MY_PREFS, MODE_PRIVATE);
+        myEditor = getSharedPreferences(MY_PREFS, MODE_PRIVATE).edit();
+
+        switch_status = myPreferences.getBoolean(SWITCH_STATUS, false);
+        anon_status = myPreferences.getBoolean(ANON_STATUS, false);
+
+        switch_anon.setChecked(switch_status);
+
+        if(anon_status) {
+            text_view_anon.setText("ON!!");
+        } else {
+            text_view_anon.setText("OFF!");
+        }
+
+        switch_anon.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean b) {
+                if (buttonView.isChecked()) {
+                    text_view_anon.setText("ON!!");
+
+                    myEditor.putBoolean(SWITCH_STATUS, true);
+                    myEditor.putBoolean(ANON_STATUS, true);
+                    myEditor.apply();
+                    switch_anon.setChecked(true);
+                } else {
+                    text_view_anon.setText("OFF!");
+
+                    myEditor.putBoolean(SWITCH_STATUS, false);
+                    myEditor.putBoolean(ANON_STATUS, false);
+                    myEditor.apply();
+                    switch_anon.setChecked(false);
+                }
+            }
+        });
 
         if (ContextCompat.checkSelfPermission(WriteMessageActivity.this,
                 Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
@@ -63,6 +156,14 @@ public class WriteMessageActivity extends AppCompatActivity {
 
     }
 
+    private void pickImageFromGallery() {
+        // intent to pick image
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, IMAGE_PICK_CODE);
+    }
+
+    // handle result of runtime permission
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch (requestCode){
@@ -86,6 +187,29 @@ public class WriteMessageActivity extends AppCompatActivity {
                     Toast.makeText(WriteMessageActivity.this, "No permission granted", Toast.LENGTH_SHORT).show();
                 }
             }
+            // adding image
+            case PERMISSION_CODE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted
+                    pickImageFromGallery();
+                }
+                else {
+                    // permission was denied
+                    Toast.makeText(this, "Permission denied...!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    // handle result of picked image
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == IMAGE_PICK_CODE) {
+            // set image to image view
+            added_image_view.setImageURI(data.getData());
         }
     }
 
