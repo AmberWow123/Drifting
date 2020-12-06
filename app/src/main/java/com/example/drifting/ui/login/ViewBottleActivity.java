3a3a5bd6e9ee744ce80c94c49423f153db06d04b
Jiaming Yang <alex760723501@gmail.com>
2020-12-06 02:12:34 -0800

    Revert "Revert "1""
    This reverts commit 59e70620850b23b712f6c170468ba1bdd430ad2f.

--- app/src/main/java/com/example/drifting/ui/login/ViewBottleActivity.java
+++ app/src/main/java/com/example/drifting/ui/login/ViewBottleActivity.java
@@ -3,6 +3,7 @@ package com.example.drifting.ui.login;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -67,7 +68,7 @@ public class ViewBottleActivity extends AppCompatActivity {
                  fromUser = "Anonymous";
              }
              pictureURL = HomeFragment.currBottle.pictureDownloadURL;
-             //Log.d("feafiawn",pictureURL);
+             if(pictureURL!=null) Log.d("feafiawn",pictureURL);
              videoURL = HomeFragment.currBottle.videoDownloadURL;
         }
 
