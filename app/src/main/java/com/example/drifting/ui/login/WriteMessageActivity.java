3c4ce7312872eacb694d2fd9ad2eabe90eef11e6
Vickie <yuc43434@gmail.com>
2020-12-06 21:16:22 -0800

    small bug fixed

--- app/src/main/java/com/example/drifting/ui/login/WriteMessageActivity.java
+++ app/src/main/java/com/example/drifting/ui/login/WriteMessageActivity.java
@@ -172,8 +172,10 @@ public class WriteMessageActivity extends AppCompatActivity {
 
                     //save the bottle id in user's send list
                     DatabaseReference UserRef = FirebaseDatabase.getInstance().getReference().child("user").child(userID);
-                    final DatabaseReference added_bottle= UserRef.child("send_list");
-                    added_bottle.setValue(true);
+                    final DatabaseReference added_bottle = UserRef.child("send_list");
+                    Map<String, Object> user_update = new HashMap<>();
+                    user_update.put(bottleID, true);
+                    added_bottle.updateChildren(user_update);
 
                     SetDatabase set = new SetDatabase();
                     if (isVideo) {
