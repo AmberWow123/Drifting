14bf060246770ffb4d61f959967fc47e4a4629da
Tianye <t4fan@ucsd.edu>
2020-12-07 02:23:05 -0800

    Update ChatActivity.java

--- app/src/main/java/com/example/drifting/ui/login/ChatActivity.java
+++ app/src/main/java/com/example/drifting/ui/login/ChatActivity.java
@@ -44,6 +44,7 @@ public class ChatActivity extends AppCompatActivity {
         userName_textview = findViewById(R.id.textView2);
 
         userName_textview.setText(ExampleAdapter.userName);
+<<<<<<< Updated upstream
         TextView textView1 = new TextView(this);
         LayoutParams layoutParams1 = new LayoutParams(LayoutParams.WRAP_CONTENT,
                 LayoutParams.WRAP_CONTENT);
@@ -57,6 +58,65 @@ public class ChatActivity extends AppCompatActivity {
         textView1.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
         textView1.setPadding(35, 20, 35, 20);
         linearLayout.addView(textView1);
+=======
+        friend_id = ExampleAdapter.friend_id;
+        chat_messages = ExampleAdapter.chat_messages;
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        //filter out the specific chats using chat_messages
+        for (int i = 0; i < chat_messages.size(); i++) {
+            Chat chat = chat_messages.get(i);
+            String this_sender = chat.getSender();
+            String this_receiver = chat.getReceiver();
+            //get the specific info!
+            if (this_receiver.equals(firebaseUser.getUid()) && this_sender.equals(friend_id)
+                    || this_receiver.equals(friend_id) && this_sender.equals(firebaseUser.getUid())) {
+                mychat.add(chat);
+            }
+        }
+
+        //loop to display all messages
+        for (int i = 0; i < mychat.size(); i++) {
+            Chat chat = mychat.get(i);
+            String this_sender = chat.getSender();
+            String this_receiver = chat.getReceiver();
+            String message = chat.getMessage();
+
+            // I send the message
+            if (this_sender.equals(firebaseUser.getUid())) {
+                TextView textView2 = new TextView(ChatActivity.this);
+                LayoutParams layoutParams = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+                layoutParams.gravity = Gravity.RIGHT;
+                layoutParams.setMargins(50, 20, 50, 20);
+                textView2.setLayoutParams(layoutParams);
+                textView2.setText(message);
+                textView2.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
+                textView2.setTextColor(0xFF000000);
+                textView2.setPadding(35, 20, 35, 20);
+                textView2.setBackgroundResource(R.drawable.border8);
+                linearLayout.addView(textView2);
+                scroll.scrollTo(0, scroll.getBottom());
+            }
+
+            //I got the message
+            else {
+                TextView textView1 = new TextView(this);
+                LayoutParams layoutParams1 = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+                layoutParams1.gravity = Gravity.LEFT;
+                layoutParams1.setMargins(50, 20, 50, 20);
+                textView1.setLayoutParams(layoutParams1);
+                textView1.setText(message);
+                textView1.setTextColor(0xFF000000);
+                textView1.setBackgroundColor(0xff66ff66); // hex color 0xAARRGGBB
+                textView1.setBackgroundResource(R.drawable.border9);
+                textView1.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
+                textView1.setPadding(35, 20, 35, 20);
+                linearLayout.addView(textView1);
+            }
+
+        }
+
+>>>>>>> Stashed changes
 
         sendMessage.setOnClickListener(new Button.OnClickListener() {
             @Override
