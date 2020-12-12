60859409003876eb71f215a3c257e1f3985d2e83
Zhengxuan Li <z3li@ucsd.edu>
2020-12-12 16:20:12 +0800

    update

--- app/src/main/java/com/example/drifting/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/drifting/ui/login/LoginActivity.java
@@ -34,7 +34,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import java.util.ArrayList;
 
 import backend.util.authentication.CredentialAuthenticator;
 import backend.util.connectivity.ConnectionChecker;
